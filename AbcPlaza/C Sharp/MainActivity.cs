743bcf92cd461e2d1fe95f632a2541a5fff2d219
Mai Văn Tuấn <40934221+mvtuan@users.noreply.github.com>
2019-11-02 17:00:25 +0700

    First commit! Create client app

---
+++ AbcPlaza/C Sharp/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using AbcPlaza.C_Sharp;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Android.App;
+
+namespace AbcPlaza
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView textMessage;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            textMessage = FindViewById<TextView>(Resource.Id.message);
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+
+            navigation.NavigationItemSelected += BottomNavigation_NavigationItemSelected;
+
+            loadFragment(Resource.Id.navigation_service);
+        }
+
+        private void BottomNavigation_NavigationItemSelected(object sender, BottomNavigationView.NavigationItemSelectedEventArgs e)
+        {
+            loadFragment(e.Item.ItemId);
+        }
+
+        private void loadFragment(int id)
+        {
+            //load fragment
+            Android.Support.V4.App.Fragment fragment = null;
+            switch(id)
+            {
+                case Resource.Id.navigation_service:
+                    fragment = ServiceFragment.NewInstance();
+                    break;
+                case Resource.Id.navigation_apartment:
+                    fragment = ApartmentFragment.NewInstance();
+                    break;
+                case Resource.Id.navigation_bills:
+                    fragment = BillFragment.NewInstance();
+                    break;
+                case Resource.Id.navigation_notifications:
+                    fragment = NotificationFragment.NewInstance();
+                    break;
+                case Resource.Id.navigation_account:
+                    fragment = AccountFragment.NewInstance();
+                    break;
+            }
+            if (fragment == null)
+                return;
+            SupportFragmentManager.BeginTransaction().Replace(Resource.Id.content_frame, fragment).Commit();
+        }
+    }
+}
+
